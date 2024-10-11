ec6b30fa7a48e71dbac45c57a10dab0b47d752eb
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-12 01:29:22 +0600

    loadvideos, displayvideos and display them on webpage

--- js/categories.js
+++ js/categories.js
@@ -49,7 +49,7 @@ const displayCategories2 = (categories) => {
         const button = document.createElement("button");
 
         // assign a class to the button
-        button.classList = "btn bg-red-400";
+        button.classList = "btn bg-red-400 rounded-full";
 
         // button names
         button.innerText = item.category;
@@ -69,11 +69,50 @@ const loadVideos = () => {
         .catch(error => console.log(error))
 }
 
+const cardDemo =
+{
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+
+
+
 // displayVideos
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById("videos");
     videos.forEach(video => {
         console.log(video);
+        const card = document.createElement("div");
+        card.classList = "card card-compact"
+        card.innerHTML = `
+        <figure>
+        <img
+         src="${video.thumbnail}
+         alt="Shoes" />
+       </figure>
+       <div class="card-body">
+           <h2 class="card-title">Shoes!</h2>
+           <p>If a dog chews shoes whose shoes does he choose?</p>
+           <div class="card-actions justify-end">
+           <button class="btn btn-primary">Buy Now</button>
+           </div>
+       </div>
+        `
+        videoContainer.appendChild(card);
     })
 }
 
