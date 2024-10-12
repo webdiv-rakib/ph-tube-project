f68a5411db916cf509c1c53f5eaf3b5bfca89638
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-13 03:01:13 +0600

    summery added about this project

---
+++ js/summery.js
@@ -0,0 +1,13 @@
+/**
+ * 1.   load and display data initially
+ * 2.   on click lead and display data
+ * 3.   complex object accessing
+ * 4.   toggle active class functionality
+ * 5.   search functionality
+ * 6.   load and display single data
+ * 7.   display data in a modal
+ * 8.   load and display single  category data
+ * 9.   Conditional Rendering: if array don't have any data
+ * 10.  Conditional Rendering: if a value empty
+ * 11.  COnditional Rendering: if a value length is zero
+ */
